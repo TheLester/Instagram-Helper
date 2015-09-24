8c54510c1b1928d3c43185f0cbaad01a4b4481c2
Dmitry Dogar <dogar@steelkiwi.com>
2015-09-24 18:31:32 +0300

    dialog improvements

--- instagramhelper/src/main/java/com/steelkiwi/instagramhelper/InstagramWebDialog.java
+++ instagramhelper/src/main/java/com/steelkiwi/instagramhelper/InstagramWebDialog.java
@@ -1,6 +1,6 @@
 package com.steelkiwi.instagramhelper;
 
-import android.app.Dialog;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -17,20 +17,22 @@ import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
-public class InstagramWebDialog extends Dialog {
-    public static final  String                   ACCESS_TOKEN = "access_token";
-    public static final  String                   ERROR        = "error";
-    public static final  String                   EQUAL        = "=";
-    static final         FrameLayout.LayoutParams FILL         = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+public class InstagramWebDialog extends AlertDialog {
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String ERROR        = "error";
+    private static final String EQUAL        = "=";
+    private static final String TAG          = InstagramWebDialog.class.getSimpleName();
+
+    private static final FrameLayout.LayoutParams FILL = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT);
-    private static final String                   TAG          = InstagramWebDialog.class.getSimpleName();
-    private ProgressDialog                    mSpinner;
-    private WebView                           mWebView;
-    private LinearLayout                      mContent;
+
+    private ProgressDialog mSpinner;
+    private WebView        mWebView;
+
     private String                            mAuthUrl;
     private String                            mRedirectUri;
     private InstagramHelper.AuthorizeListener mListener;
-
+    private LinearLayout                      mContent;
 
     public InstagramWebDialog(Context context, String authUrl, String redirectUri, InstagramHelper.AuthorizeListener listener) {
         super(context);
@@ -40,27 +42,24 @@ public class InstagramWebDialog extends Dialog {
         mRedirectUri = redirectUri;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mSpinner = new ProgressDialog(getContext());
-
         mSpinner.requestWindowFeature(Window.FEATURE_NO_TITLE);
         mSpinner.setMessage("Loading...");
 
-        mContent = new LinearLayout(getContext());
-        mContent.setOrientation(LinearLayout.VERTICAL);
-
+        setUpContent();
         setUpWebView();
+
+
+        double[] dimensions = new double[2];
 
         Display display = getWindow().getWindowManager().getDefaultDisplay();
         Point outSize = new Point();
 
         int width = 0;
         int height = 0;
-
-        double[] dimensions = new double[2];
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
             display.getSize(outSize);
@@ -72,15 +71,16 @@ public class InstagramWebDialog extends Dialog {
             height = display.getHeight();
         }
 
-        if (width < height) {
-            dimensions[0] = 0.87 * width;
-            dimensions[1] = 0.82 * height;
-        } else {
-            dimensions[0] = 0.75 * width;
-            dimensions[1] = 0.75 * height;
-        }
+        dimensions[0] = width;
+        dimensions[1] = height;
 
         addContentView(mContent, new FrameLayout.LayoutParams((int) dimensions[0], (int) dimensions[1]));
+    }
+
+
+    private void setUpContent() {
+        mContent = new LinearLayout(getContext());
+        mContent.setOrientation(LinearLayout.VERTICAL);
     }
 
 
@@ -89,17 +89,29 @@ public class InstagramWebDialog extends Dialog {
 
         mWebView.setVerticalScrollBarEnabled(false);
         mWebView.setHorizontalScrollBarEnabled(false);
-        mWebView.setWebViewClient(new InstagramWebViewClient());
+        mWebView.setWebViewClient(new LoginWebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.loadUrl(mAuthUrl);
-        mWebView.setLayoutParams(FILL);
-
         WebSettings webSettings = mWebView.getSettings();
-
         webSettings.setSavePassword(false);
         webSettings.setSaveFormData(false);
-
-        mContent.addView(mWebView);
+        mWebView.setLayoutParams(FILL);
+//        mWebView.requestFocus(View.FOCUS_DOWN);
+//        mWebView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                switch (event.getAction()) {
+//                    case MotionEvent.ACTION_DOWN:
+//                    case MotionEvent.ACTION_UP:
+//                        if (!v.hasFocus()) {
+//                            v.requestFocus();
+//                        }
+//                        break;
+//                }
+//                return false;
+//            }
+//        });
+//        mContent.addView(mWebView);
     }
 
     public void clearCache() {
@@ -115,7 +127,7 @@ public class InstagramWebDialog extends Dialog {
 
     }
 
-    private class InstagramWebViewClient extends WebViewClient {
+    private class LoginWebViewClient extends WebViewClient {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {

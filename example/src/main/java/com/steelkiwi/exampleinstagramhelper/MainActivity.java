25dc4c9389d8d210681812a18a03b05055d08d4c
Dmitry Dogar <dogar@steelkiwi.com>
2015-09-25 15:54:24 +0300

    improve example usage

--- example/src/main/java/com/steelkiwi/exampleinstagramhelper/MainActivity.java
+++ example/src/main/java/com/steelkiwi/exampleinstagramhelper/MainActivity.java
@@ -5,34 +5,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+import com.steelkiwi.instagramhelper.InstagramHelper;
 import com.steelkiwi.instagramhelper.InstagramHelperConstants;
 import com.steelkiwi.instagramhelper.model.InstagramUser;
 
 public class MainActivity extends Activity {
+    private InstagramHelper instagramHelper;
+    private LinearLayout    userInfoPanel;
+    private ImageView       userPhoto;
+    private TextView        userTextInfo;
+    private Button          loginBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-        Button login = (Button) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+        instagramHelper = TestApplication.getInstagramHelper();
+
+        userInfoPanel = (LinearLayout) findViewById(R.id.user_instagram_info);
+        userPhoto = (ImageView) findViewById(R.id.user_photo);
+        userTextInfo = (TextView) findViewById(R.id.user_text_info);
+        loginBtn = (Button) findViewById(R.id.btn_login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TestApplication.getInstagramHelper().loginFromActivity(MainActivity.this);
+                instagramHelper.loginFromActivity(MainActivity.this);
             }
         });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == InstagramHelperConstants.INSTA_LOGIN && resultCode == RESULT_OK) {
-            Toast.makeText(this,"OK",Toast.LENGTH_LONG).show();
-            InstagramUser user = TestApplication.getInstagramHelper().getInstagramUser(this);
+            InstagramUser user = instagramHelper.getInstagramUser(this);
+            loginBtn.setVisibility(View.GONE);
+            userInfoPanel.setVisibility(View.VISIBLE);
+            Picasso.with(this).load(user.getData().getProfilePicture()).into(userPhoto);
+            userTextInfo.setText(user.getData().getUsername() + "\n"
+                            + user.getData().getFullName() + "\n"
+                            + user.getData().getWebsite()
+            );
 
         } else {
-            Toast.makeText(this,"FAIL",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Login failed", Toast.LENGTH_LONG).show();
         }
     }
 }
